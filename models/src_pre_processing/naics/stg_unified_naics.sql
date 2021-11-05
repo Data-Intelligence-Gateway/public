158fdc48a5ffd684685941d384708ac4f441b949
joey@aprcot.ai <joey@aprcot.ai>
2021-11-05 15:44:11 +0000

    Dims: build NAICS codes dim table

---
+++ models/src_pre_processing/naics/stg_unified_naics.sql
@@ -0,0 +1,5 @@
+{{ config(materialized='ephemeral')}}
+
+{%- set naics_rels = dbt_utils.get_relations_by_pattern(target.schema, 'naics%') -%}
+{%- set cols_overrid = {'code': 'text', 'title': 'text', 'description':'text'} -%}
+{{ dbt_utils.union_relations(relations = naics_rels, column_override=cols_overrid) }}
