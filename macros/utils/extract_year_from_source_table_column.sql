b73d3d18890c4468427935f1eebd67678d02af0d
joeyfezster <joey.baruch@gmail.com>
2021-11-07 00:10:22 -0400

    Macros: reorganize into folders
    * add source_table with default value to extract_year_from_source_table_column

--- macros/utils/extract_year_from_source_table_column.sql
+++
@@ -1,4 +0,0 @@
-{% macro extract_year_from_source_table_column() -%}
-{#- https://stackoverflow.com/a/40564710/3517025 -#}
-    regexp_replace({{ var('source_table') }}, '\D','','g')
-{%- endmacro %}
